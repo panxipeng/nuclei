64d26b4bbb9ef2dc48cd51a89f191385dbe7af65
Ilya Dobrynin <iliadobrynin@yandex.ru>
2018-02-07 18:05:10 +0300

    Make dirs func add

--- dirs.py
+++ dirs.py
@@ -1,3 +1,14 @@
 import os
 
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+
+
+def make_dir(relative_path):
+    dirs = relative_path.split('/')
+    top_dir = ROOT_DIR
+    for dir in dirs:
+        top_dir = os.path.join(top_dir, dir)
+        if not os.path.exists(top_dir):
+            os.mkdir(top_dir)
+
+    return top_dir
